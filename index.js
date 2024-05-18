73799a08f63c4f9f5a569f2e5c3278292e441143
Eric <ericpeterson@tamu.edu>
2024-05-17 23:00:05 -0500

    3.3

--- index.js
+++ index.js
@@ -30,10 +30,23 @@ app.get('/api/persons', (request, response) => {
     response.json(data)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = data.find(item => item.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+        console.log(`404 at ${id}`)
+    }
+})
+
 app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${data.length} people <br> <br>
     ${(new Date()).toString()}`)
 })
+
 
 
 const PORT = 3002;
