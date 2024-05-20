46b7540088f82f56ae611e93574afd42975c0beb
Eric <ericpeterson@tamu.edu>
2024-05-19 20:18:31 -0500

    added dist

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ morgan.token('body', (req, res) => {
 app.use(express.json())
 app.use( morgan(':method :url :status :res[content-length] - :response-time ms :body') )
 app.use(cors())
+app.use(express.static('dist'))
 
 var data = [
     { 
